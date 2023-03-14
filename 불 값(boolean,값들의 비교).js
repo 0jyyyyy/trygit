8c4b01655d7e02a11a1bba4bb1ccaaaa35a4469a
0jyyyyy <ojy0533@naver.com>
2023-03-14 22:29:37 +0900

    연산자 우선순위와 불 값

---
+++ 불 값(boolean,값들의 비교).js
@@ -0,0 +1,8 @@
+console.log(5<4); //false
+console.log(5>4); //true
+console.log(5>=4); //true
+console.log(5<=4); //false
+console.log(5==4); //false
+console.log(5==5); //true
+console.log(5!=4); //true
+console.log(NaN==NaN); //false
