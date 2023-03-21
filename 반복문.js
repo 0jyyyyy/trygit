ad3fc0ac455e535153712de67dd1568e49719f1f
0jyyyyy <ojy0533@naver.com>
2023-03-21 23:04:16 +0900

    반복문

---
+++ 반복문.js
@@ -0,0 +1,53 @@
+//while 문으로 Hello, while! 100번 출력하기
+
+//while(조건식)
+//  동작문;
+
+
+/* 무한 반복문
+while(true){
+    console.log('Hello,while');
+}
+*/
+
+/*
+let i = 1;
+while(i <=100){
+    console.log('Hello,while');
+    i++;
+}
+*/
+
+//for문으로 반복해서 출력
+
+//for(시작; 조건식; 종료식)
+//    동작문;
+
+/*
+for(let i = 0; i < 100; i++){
+    console.log('Hello, for');
+}
+*/
+
+//for문의 시작, 조건식, 종료식은 생략할수 있다.
+//for(;;){
+
+//} => 무한반복
+
+//1 ~ 100까지 출력
+/*
+let i = 1;
+while(i<=100){
+    console.log(i);
+    i++;
+}
+*/
+/* 퀴즈 1 ~ 100 까지 for문으로 출력
+for(let i =1; i <= 100; i++){
+    console.log(i);
+}
+for(let i =0; i < 100; i++){
+    console.log(i+1);
+}
+
+*/
