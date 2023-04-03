c62663c9bb5d14c476a5342d17650cdcea6333b3
0jyyyyy <ojy0533@naver.com>
2023-04-03 23:12:21 +0900

    이벤트 리스너

---
+++ 2강 순서도 그리기와 HTML화면 만들기/이벤트 리스너 달기(콜백함수).js
@@ -0,0 +1,30 @@
+//이벤트 리스너
+//입력 이벤트
+/*document.querySelector('input').addEventListener('input',function(){
+
+});
+*/
+
+//클릭 이벤트
+/**
+ * document.querySelector('button').addEventListener('click',onClickButton)
+ */
+
+//코드 사용법
+/**
+ * 태그.addEventListener('이벤트 이름', 리스너 함수);
+ * 
+ */
+
+//콜백 함수
+//연이어서 실행되는 함수를 콜백 함수라고 한다.
+
+//퀴즈
+//이벤트를 달 때 사용하는 메서드는 무엇인가? 3번
+/**
+ * 1. querySelector : 태그를 선택할때
+ * 2. addListener
+ * 3. addEventListener   
+ * 4. removeListener : 이벤트 리스너를 삭제할때
+ * 
+ */
