087044e7be21f0b9e42862b31bdbe17f3898d13d
0jyyyyy <ojy0533@naver.com>
2023-04-04 22:52:50 +0900

    순서도 최적화하기

---
+++ 2강 순서도 그리기와 HTML화면 만들기/틀렸을때 오류표시하기.js
@@ -0,0 +1,12 @@
+//퀴즈
+// 다음 태그들의 내부 값을 가져올 때 둘 중 어떤 속성을 사용해야 하는지 표시
+/**
+ * 1. input (value / textContent) : v 정답
+ * 2. button (value / textContent) : t 정답
+ * 3. select (value / textContent) : v 정답
+ * 4. div (value / textContent) : v t
+ * 5. textarea (value / textContent) : t v
+ * 6. span (value / textContent) : v t
+ * 
+ * 무엇인가 입력하는 것들은 대부분 value
+*/
