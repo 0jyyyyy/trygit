d657180557805e20c52d95171dd1721481ea69dd
0jyyyyy <ojy0533@naver.com>
2023-04-10 23:44:04 +0900

    고차함수와 if문 중첩 줄이기

---
+++ 3강 계산기/고차함수.js
@@ -0,0 +1,13 @@
+//퀴즈
+/**
+ *  다음 코드의 console.log 결과를 맞춰보세요
+ * 
+ * const hof = (a) => (b) => (c) => {
+ *  return a + (b * c)
+ * };
+ * 
+ * const first = hof(3);
+ * const second = first(4);
+ * const third = second(5);
+ * console.log(third); 23?
+ */
