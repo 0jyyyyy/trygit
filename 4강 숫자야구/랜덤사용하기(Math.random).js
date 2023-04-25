6a682b12e0960ee6f28d97d1e7090b3a53dafb95
0jyyyyy <ojy0533@naver.com>
2023-04-25 21:24:34 +0900

    숫자야구 랜덤이용해서 값 만들기

---
+++ 4강 숫자야구/랜덤사용하기(Math.random).js
@@ -0,0 +1,8 @@
+/**
+ * 무작위로 숫자 뽑기
+ * 
+ * 무작위 숫자를 만드는 함수는 Math.random() 이다.
+ *  Math.random() * 9   ==> 0.xx <= Math.random() * 9 < 9.xx
+ *  Math.random() * 9 + 1   ==> 1.xx <= Math.random() * 9 + 1 < 10.xx
+ *  Math.floor(Math.random() * 9 + 1)   ==> 1 <= Math.floor(Math.random() * 9 + 1 < 10
+ */
